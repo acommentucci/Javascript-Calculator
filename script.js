5ff069d6401a2957a1b491ef8df632baeda88b5f
acommentucci <a.commentucci@gmail.com>
2021-03-20 21:13:32 +0000

    script.js added

---
+++ script.js
@@ -0,0 +1,67 @@
+// Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = document.getElementsByTagName("button");
+
+  for(let button of buttons) {
+    button.addEventListener("click", function() {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        runGame(gameType);
+      }
+    })
+  }
+
+  runGame("addition");
+})
+
+function runGame(gameType) {
+
+  // Generate two random numbers between 1 and 25
+  // Math.floor rounds down to the whole number
+  // Math.random generates random numbers
+
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type ${gameType}`);
+    throw `Unknown game type ${gameType}, aborting!`
+  }
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "+";
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
