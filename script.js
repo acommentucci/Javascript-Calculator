c8c0b26570926d8c6618d82a1365930496a06854
acommentucci <a.commentucci@gmail.com>
2021-03-21 12:03:16 +0000

    user friendly functionality added

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
     })
   }
 
+  document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      checkAnswer();
+    }
+  })
+
   runGame("addition");
 })
 
@@ -24,13 +30,18 @@ function runGame(gameType) {
   // Math.floor rounds down to the whole number
   // Math.random generates random numbers
 
+  document.getElementById("answer-box").value="";
+  document.getElementById("answer-box").focus();
+
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
 
   if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
   } else if (gameType === "multiply") {
-    displayMultiplyQuestion(num1, num2)
+    displayMultiplyQuestion(num1, num2);
+  } else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
   } else {
     alert(`Unknown game type ${gameType}`);
     throw `Unknown game type ${gameType}, aborting!`
@@ -71,6 +82,8 @@ function calculateCorrectAnswer() {
     return [operand1 + operand2, "addition"];
   } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
+  } else if (operator === "-") {
+    return [operand1 - operand2, "subtract"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}, aborting!`;
@@ -101,8 +114,10 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1 > operand2 ?operand1 : operand2;
+  document.getElementById("operand2").textContent = operand1 > operand2 ?operand2 : operand1;
+  document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
